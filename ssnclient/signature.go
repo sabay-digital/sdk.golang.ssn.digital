966d5dca9bef035755d3ca9c4f482a1c55cc08f2
Jacob Owen <jacob.owen@sabay.com>
2020-01-17 11:33:20 +0700

    Add response structs

--- ssnclient/signature.go
+++ ssnclient/signature.go
@@ -14,11 +14,17 @@ import (
 *
  */
 
-// VerifySignatureRequest describes the JSON structure for making a request to verify signature API
+// VerifySignatureRequest describes the JSON structure for making a request to the verify signature API
 type VerifySignatureRequest struct {
 	Public_key string `json:"public_key"`
 	Signature  string `json:"signature"`
 	Message    string `json:"message"`
+}
+
+// VerifySignatureResponse describes the JSON structure for the response from the verify signature API
+type VerifySignatureResponse struct {
+	Status int    `json:"status"`
+	Title  string `json:"title"`
 }
 
 // VerifySignature checks whether the provided message, signature and public key are valid
@@ -40,7 +46,7 @@ func VerifySignature(message, signature, publicKey, api string) (bool, error) {
 		return false, err
 	}
 	vsReq.Header.Add("Content-Type", "application/json")
-	
+
 	vsResp, err := http.DefaultClient.Do(vsReq)
 	if ssn.Log(err, "VerifySignature: Send HTTP request") {
 		return false, err
